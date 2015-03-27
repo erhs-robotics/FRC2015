a72842bca3c4bd2b2a10ca99e3e850d2fb2a320e
Michael Stevens <mls0.5mv2@gmail.com>
2015-03-26 23:19:30 -0700

    basic automode

--- src/org/usfirst/frc/team53/robot/util/Utils.java
+++ src/org/usfirst/frc/team53/robot/util/Utils.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team53.robot.util;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 public class Utils {
 	public static Command runnable2Command(Runnable run) {
@@ -10,6 +11,22 @@ public class Utils {
 			protected void interrupted() { }			
 			@Override
 			protected void initialize() { run.run(); }			
+			@Override
+			protected void execute() { }			
+			@Override
+			protected void end() { }
+		};
+	}
+	
+	public static Command delay(double seconds) {
+		return new Command() {
+			double mStartTime;
+			@Override
+			protected boolean isFinished() { return (Timer.getFPGATimestamp() - mStartTime) >= seconds; }			
+			@Override
+			protected void interrupted() { }			
+			@Override
+			protected void initialize() { mStartTime = Timer.getFPGATimestamp(); }			
 			@Override
 			protected void execute() { }			
 			@Override
